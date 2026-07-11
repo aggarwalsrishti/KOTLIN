c41d5e30527fc4a75616c8e9c933708e87dcb26a
SRISHTI AGGARWAL <srishtiaggarwal1303@gmail.com>
2026-07-11 17:37:51 +0530

    Delete src directory

--- src/Arrays.kt
+++
@@ -1,9 +0,0 @@
-fun main(){
-    var arr= arrayOf(85,90,50,30,70,60)
-    var a=1
-    for (i in arr){
-        println("Student ${a++} marks : $i")
-
-    }
-
-}
--- src/WhenStatement.kt
+++
@@ -1,9 +0,0 @@
-fun main(){
-    val time="Morning"
-    when(time){
-    "Morning" -> {println("Breakfast")}
-    "Afternoon" -> {println("Lunch")}
-    "Evening" -> {println("Dinner")}
-    else -> {println("GreatDay")}
-    }
-}
